3c10bc6f8ee0ab344cfd887b3a65d39921e41775
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-06-11 16:03:17 -0600

    normalize libraries
    css normalization
    👌🏽nanoid
    👌🏽notiflix
    👌🏽styled components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notify.init({
+  width: '300px',
+  position: 'center-top',
+  clickToClose: false,
+  useIcon: true,
+  pauseOnHover: true,
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
